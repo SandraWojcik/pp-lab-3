d1571759a491920d9eb11e2095bc126479aba326
SandraWojcik <wojcisa3728_aehit@students.vizja.pl>
2024-04-06 08:42:05 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import geometry.Circle;
+import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
@@ -6,13 +6,14 @@ public class Main {
         // Tworzenie obiektu klasy Point
         Point center = new Point(0, 0);
 
-        // Tworzenie obiektu klasy Circle i inicjowanie jego wartości
-        Circle circle = new Circle(center, 5.0);
+        // Tworzenie obiektu klasy ColoredCircle i inicjowanie jego wartości
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
 
-        // Wyświetlanie właściwości koła
-        System.out.println("Właściwości koła:");
-        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + circle.getRadius());
-        System.out.println("Obwód koła: " + circle.calculatePerimeter());
+        // Wyświetlanie właściwości kolorowego koła
+        System.out.println("Właściwości kolorowego koła:");
+        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + coloredCircle.getRadius());
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
+        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
     }
 }
